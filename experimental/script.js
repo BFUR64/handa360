6aca15694c4df90447631e8dedd852a5838105d4
Terrance1120 <iterranceevan@gmail.com>
2026-03-18 07:51:24 +0800

    Added experimental code

---
+++ experimental/script.js
@@ -0,0 +1,15 @@
+addEventListener("DOMContentLoaded", function() {
+    init();
+});
+
+const data = {
+    name: "Terrance",
+    age: 30,
+    isStudent: false,
+    hobbies: ["programming", "coding"]
+};
+
+function init() {
+    document.getElementById("edit").innerText = "BRRR";
+    // console.dir(data);
+}
