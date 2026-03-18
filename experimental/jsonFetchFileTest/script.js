aef6ae5f1ed1f3c975f51d73ec01b1227632712d
Terrance1120 <iterranceevan@gmail.com>
2026-03-18 08:32:53 +0800

    Fixed issue with 404 response on the json fetch request

--- experimental/jsonFetchFileTest/script.js
+++ experimental/jsonFetchFileTest/script.js
@@ -3,10 +3,10 @@ addEventListener("DOMContentLoaded", function() {
 });
 
 async function init() {
-    let result = await fetch("https://raw.githubusercontent.com/BFUR64/handa360/refs/heads/main/experimental/jsonFetchFileTest/data.json");
+    let result = await fetch("https://raw.githubusercontent.com/BFUR64/handa360/main/experimental/jsonFetchFileTest/data.json");
     let json = await result.json();
 
-    let currentHobbies = json.hobbies.join();
+    let currentHobbies = json.hobbies.join(", ");
 
     document.getElementById("edit").innerHTML = 
         "<h1>" + json.name + "</h1>" +
