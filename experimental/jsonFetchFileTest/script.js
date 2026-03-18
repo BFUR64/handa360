95b287766ed2bbf0f19fe10388294c403afd3bff
Terrance1120 <iterranceevan@gmail.com>
2026-03-18 08:28:59 +0800

    Added JSON Fetch Testing in Experimental

---
+++ experimental/jsonFetchFileTest/script.js
@@ -0,0 +1,16 @@
+addEventListener("DOMContentLoaded", function() {
+    init();
+});
+
+async function init() {
+    let result = await fetch("https://raw.githubusercontent.com/BFUR64/handa360/refs/heads/main/experimental/jsonFetchFileTest/data.json");
+    let json = await result.json();
+
+    let currentHobbies = json.hobbies.join();
+
+    document.getElementById("edit").innerHTML = 
+        "<h1>" + json.name + "</h1>" +
+        "<h2>" + json.age + "</h2>" +
+        "<h3>Student: " + json.isStudent + "</h3>" +
+        "<h4>Hobbies: " + currentHobbies + "</h4>";
+}
