9f1b4059dc3dfca86ccf95cb28b94a64962fd0b1
melcone <98726565+melcone@users.noreply.github.com>
2026-03-22 00:09:16 +0800

    Added the dynamic list algo and added a capability in which it can fetch data from a .json file

--- experimental/melWebTest/script.js
+++ experimental/melWebTest/script.js
@@ -6,8 +6,29 @@ queryForm.addEventListener("submit", function(event) {
     const answer1 = queryForm.question1.value;
     const answer2 = queryForm.question2.value;
 
-    console.log("Answer 1:", answer1);
-    console.log("Answer 2:", answer2);
-    
-    alert(`You chose: ${answer1} and ${answer2}`);
+    loadInstructionsToHTML(answer2);
 });
+
+function loadInstructionsToHTML(hazardType) {
+    // generate a dynamic list based on the `const data` object that relies on the
+    // `hazard` variable (see below).
+    let output = document.getElementById("output");
+    let checklistTemplate = document.getElementById("checklist-template");
+
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        for (let action = 0; action < data.actions.length; action++) {
+            let currentHazard = data.actions[action].condition.hazard;
+
+            if (currentHazard === hazardType) {
+                for (let instruction = 0; instruction < data.actions[action].instructions.length; instruction++) {
+                    // Lets you duplicate your template and show multiple checklist items.
+                    let clone = checklistTemplate.content.cloneNode(true);
+                    clone.querySelector("#checklist-item-text").innerText = data.actions[action].instructions[instruction];
+                    output.appendChild(clone);
+                }
+            }
+        }
+    })
+}
