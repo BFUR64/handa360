29418a5c577be7e0351d5cb76295f633e07073b0
Terrance1120 <iterranceevan@gmail.com>
2026-03-21 17:38:58 +0800

    Added initial json format testing for file formats

---
+++ experimental/jsonFormatTest/script.js
@@ -0,0 +1,91 @@
+const data = {
+    actions: [
+        {
+            condition: { hazard: "typhoon" },
+            instructions: [
+                "Get inside a strong building",
+                "Stay away from windows and glass",
+                "Move to safest area (interior room, lower floor not prone to flooding)",
+                "Charge phone and all power banks",
+                "Turn on radio / check official updates"
+            ]
+        },
+        {
+            condition: { hazard: "flood" },
+            instructions: [
+                "Move to higher ground or upper floor",
+                "Stay away from rising water",
+                "Turn off electricity (if safe to do so)",
+                "Monitor official alerts"
+            ]
+        },
+        {
+            condition: { hazard: "earthquake" },
+            instructions: [
+                "DROP to the ground",
+                "COVER under sturdy furniture",
+                "HOLD ON until shaking stops",
+                "Stay away from glass and heavy objects"
+            ]
+        },
+        {
+            condition: { hazard: "landslide" },
+            instructions: [
+                "Move away from slopes and hillsides",
+                "Go to higher, stable ground",
+                "Stay alert for rumbling/cracking sounds"
+            ]
+        }
+    ],
+
+    locations: [
+        {
+            condition: { location: "kalibo" },
+            information: [
+                "Police - 09xxxx",
+                "Fire Station - 09xxxx",
+                "Emergency - 09xxxx"
+            ]
+        },
+        {
+            condition: { location: "manila" },
+            information: [
+                "Police - 09xxxx",
+                "Fire Station - 09xxxx",
+                "Emergency - 09xxxx"
+            ]
+        },
+        {
+            condition: { location: "iloilo" },
+            information: [
+                "Police - 09xxxx",
+                "Fire Station - 09xxxx",
+                "Emergency - 09xxxx"
+            ]
+        }
+    ]
+};
+
+addEventListener("DOMContentLoaded", function() {
+    init();
+});
+
+function init() {
+    // generate a dynamic list based on the `const data` object that relies on the
+    // `hazard` variable (see below).
+
+    let hazard = "earthquake";
+
+    let div = document.getElementById("output");
+    let template = document.getElementById("checklist-template");
+    
+    let clone = template.content.cloneNode(true);
+
+    clone.querySelector("#checklist-item-text").innerText = "wdwadwadwadawd";
+
+    let clone2 = template.content.cloneNode(true);
+    clone2.querySelector("#checklist-item-text").innerText = "bruh";
+
+    div.appendChild(clone);
+    div.appendChild(clone2);
+}
