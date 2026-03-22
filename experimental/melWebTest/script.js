b8fb7ce603db3fe75a09bafa7348a8ace2972513
Terrance1120 <iterranceevan@gmail.com>
2026-03-22 20:23:01 +0800

    Updated to newer async/await syntax of js

--- experimental/melWebTest/script.js
+++ experimental/melWebTest/script.js
@@ -1,34 +1,34 @@
 const queryForm = document.getElementById("queryForm");
 
-queryForm.addEventListener("submit", function(event) {
+queryForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
     const answer1 = queryForm.question1.value;
     const answer2 = queryForm.question2.value;
 
-    loadInstructionsToHTML(answer2);
+    await loadInstructionsToHTML(answer2);
 });
 
-function loadInstructionsToHTML(hazardType) {
+async function loadInstructionsToHTML(hazardType) {
     // generate a dynamic list based on the `const data` object that relies on the
     // `hazard` variable (see below).
     let output = document.getElementById("output");
     let checklistTemplate = document.getElementById("checklist-template");
 
-fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-        for (let action = 0; action < data.actions.length; action++) {
-            let currentHazard = data.actions[action].condition.hazard;
+    let response = await fetch("data.json");
 
-            if (currentHazard === hazardType) {
-                for (let instruction = 0; instruction < data.actions[action].instructions.length; instruction++) {
-                    // Lets you duplicate your template and show multiple checklist items.
-                    let clone = checklistTemplate.content.cloneNode(true);
-                    clone.querySelector("#checklist-item-text").innerText = data.actions[action].instructions[instruction];
-                    output.appendChild(clone);
-                }
+    let data = await response.json();
+
+    for (let action = 0; action < data.actions.length; action++) {
+        let currentHazard = data.actions[action].condition.hazard;
+
+        if (currentHazard === hazardType) {
+            for (let instruction = 0; instruction < data.actions[action].instructions.length; instruction++) {
+            // Lets you duplicate your template and show multiple checklist items.
+                let clone = checklistTemplate.content.cloneNode(true);
+                clone.querySelector("#checklist-item-text").innerText = data.actions[action].instructions[instruction];
+                output.appendChild(clone);
             }
         }
-    })
+    }
 }
