e847fce79457d8cc7b44ca0e88ce55cddb872426
Terrance1120 <iterranceevan@gmail.com>
2026-03-23 18:09:29 +0800

    Reduced repetition via extracting helper functions

--- experimental/loopTest/cacheController.js
+++ experimental/loopTest/cacheController.js
@@ -10,82 +10,45 @@ const LOCAL_LOCATIONS_KEY = "locations";
 
 export function loadFromStorage() {
     try {
-        loadQuestionsFromStorage();
-        loadActionsFromStorage();
-        loadLocationsFromStorage();
+        loadItemFromStorage(LOCAL_QUESTIONS_KEY, cachedData.setQuestions);
+        loadItemFromStorage(LOCAL_ACTIONS_KEY, cachedData.setActions);
+        loadItemFromStorage(LOCAL_LOCATIONS_KEY, cachedData.setLocations);
     }
     catch (error) {
         console.warn("Local storage cache corrupted, skipping...", error.message);
     }
 }
 
-function loadQuestionsFromStorage() {
-    let localQuestions = localStorage.getItem(LOCAL_QUESTIONS_KEY);
+function loadItemFromStorage(itemKey, setterFunction) {
+    let localData = localStorage.getItem(itemKey);
 
-    if (localQuestions != null) {
-        cachedData.setQuestions(JSON.parse(localQuestions));
-    }
-}
-
-function loadActionsFromStorage() {
-    let localActions = localStorage.getItem(LOCAL_ACTIONS_KEY);
-
-    if (localActions != null) {
-        cachedData.setActions(JSON.parse(localActions));
-    }
-}
-
-function loadLocationsFromStorage() {
-    let localLocations = localStorage.getItem(LOCAL_LOCATIONS_KEY);
-
-    if (localLocations != null) {
-        cachedData.setLocations(JSON.parse(localLocations));
+    if (localData != null) {
+        setterFunction(JSON.parse(localData));
     }
 }
 
 export async function syncFromRemote() {
-    try {
-        await syncQuestionsFromRemote();
-        await syncActionsFromRemote();
-        await syncLocationsFromRemote();
-    }
-    catch (error) {
-        console.warn("Failed to load from remote, skipping...", error.message);
-    }
+    let results = await Promise.allSettled([
+        syncFromUrl(QUESTIONS_URL, cachedData.setQuestions, LOCAL_QUESTIONS_KEY),
+        syncFromUrl(ACTIONS_URL, cachedData.setActions, LOCAL_ACTIONS_KEY),
+        syncFromUrl(LOCATIONS_URL, cachedData.setLocations, LOCAL_LOCATIONS_KEY)
+    ]);
+
+    results.forEach(result => {
+        if (result.status === "rejected") {
+            console.log("Sync failed for the file:", result.reason.message);
+        }
+    });
 }
 
-async function syncQuestionsFromRemote() {
-    let response = await fetch(QUESTIONS_URL);
+async function syncFromUrl(url, setterFunction, key) {
+    let response = await fetch(url);
 
     if (!response.ok) {
         throw new Error(`Server said: ${response.status} ${response.statusText}`);
     }
 
-    cachedData.setQuestions(await response.json());
+    setterFunction(await response.json());
 
-    localStorage.setItem(LOCAL_QUESTIONS_KEY, JSON.stringify(cachedData.questions));
-}
-
-async function syncActionsFromRemote() {
-    let response = await fetch(ACTIONS_URL);
-
-    if (!response.ok) {
-        throw new Error(`Server said: ${response.status} ${response.statusText}`);
-    }
-
-    cachedData.setActions(await response.json());
-
-    localStorage.setItem(LOCAL_ACTIONS_KEY, JSON.stringify(cachedData.actions));
-}
-
-async function syncLocationsFromRemote() {
-    let response = await fetch(LOCATIONS_URL);
-
-    if (!response.ok) {
-        throw new Error(`Server said: ${response.status} ${response.statusText}`);
-    }
-
-    cachedData.setLocations(await response.json());
-
-    localStorage.setItem(LOCAL_LOCATIONS_KEY, JSON.stringify(cachedData.locations));
+    localStorage.setItem(key, JSON.stringify(target));
 }
