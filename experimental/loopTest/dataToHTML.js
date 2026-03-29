02906898fc2981c0906c7558891bc1a99c47e45a
Terrance1120 <iterranceevan@gmail.com>
2026-03-29 15:32:45 +0800

    Added form and button submit to printQuestions() for index.html and template for the form

--- experimental/loopTest/dataToHTML.js
+++ experimental/loopTest/dataToHTML.js
@@ -2,12 +2,14 @@
 
 import * as cachedData from "./cachedData.js";
 
+const formTemplate = document.getElementById("form-template");
 const questionTemplate = document.getElementById("question-template");
 const optionTemplate = document.getElementById("option-template");
 const container = document.getElementById("container");
 
 export function printQuestions() {
     if (
+        !(formTemplate instanceof HTMLTemplateElement) ||
         !(questionTemplate instanceof HTMLTemplateElement) ||
         !(optionTemplate instanceof HTMLTemplateElement) ||
         !(container instanceof HTMLElement)
@@ -16,37 +18,48 @@ export function printQuestions() {
     let questions = cachedData.getQuestions();
     if (!questions) return;
 
+    let formTemplateClone = formTemplate.content.cloneNode(true);
+    if (!(formTemplateClone instanceof DocumentFragment)) return;
+
+    let formBlock = formTemplateClone.querySelector(".form-block");
+    if (!formBlock) return;
+
+    let formButton = formTemplateClone.querySelector(".btn-submit");
+    if (!formButton) return;
+
     questions.forEach(question => {
 
-        let qClone = (questionTemplate.content.cloneNode(true));
-        if (!(qClone instanceof DocumentFragment)) return;
+        let questionTemplateClone = questionTemplate.content.cloneNode(true);
+        if (!(questionTemplateClone instanceof DocumentFragment)) return;
 
-        let textElement = qClone.querySelector(".question-text");
+        let textElement = questionTemplateClone.querySelector(".question-text");
         if (!textElement) return;
 
         textElement.textContent = question.text;
 
-        let questionSelection = qClone.querySelector(".question-block");
+        let questionSelection = questionTemplateClone.querySelector(".question-block");
         if (!questionSelection) return;
 
         question.options.forEach(option => {
-            let oClone = (optionTemplate.content.cloneNode(true));
-            if (!(oClone instanceof DocumentFragment)) return;
+            let optionTemplateClone = optionTemplate.content.cloneNode(true);
+            if (!(optionTemplateClone instanceof DocumentFragment)) return;
 
-            let radioInput = (oClone.querySelector("input[type='radio']"));
+            let radioInput = (optionTemplateClone.querySelector("input[type='radio']"));
             if (!(radioInput instanceof HTMLInputElement)) return;
 
             radioInput.value = option.value;
             radioInput.name = question.name;
 
-            let buttonText = oClone.querySelector(".option-button");
+            let buttonText = optionTemplateClone.querySelector(".option-button");
             if (!buttonText) return;
 
             buttonText.textContent = option.text;
 
-            questionSelection.append(oClone);
+            questionSelection.append(optionTemplateClone);
         })
 
-        container.append(qClone);
+        formButton.before(questionTemplateClone);
     })
+
+    container.append(formBlock);
 }
