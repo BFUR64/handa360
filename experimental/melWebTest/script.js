f8651b8f23d658fb05124a144ce98fc04d91b5c3
melcone <98726565+melcone@users.noreply.github.com>
2026-03-29 20:04:47 +0800

    Finally added logic for getting contact list

--- experimental/melWebTest/script.js
+++ experimental/melWebTest/script.js
@@ -6,31 +6,61 @@ queryForm.addEventListener("submit", async function(event) {
     const answer1 = queryForm.question1.value;
     const answer2 = queryForm.question2.value;
 
-    await loadInstructionsToHTML(answer2);
+    await loadInstructionsToHTML(answer1, answer2);
 });
 
-async function loadInstructionsToHTML(hazardType) {
+async function loadInstructionsToHTML(specificLocation, hazardType) {
     // generate a dynamic list based on the `const data` object that relies on the
     // `hazard` variable (see below).
-    let output = document.getElementById("output");
+    let hazardInstructionsOutput = document.getElementById("hazard-instructions-output");
     let checklistTemplate = document.getElementById("checklist-template");
 
+    let locationContactsOutput = document.getElementById("location-contacts-output");
+    let contactlistsTemplate = document.getElementById("contactslist-template");
+
      // Stops checklist from stacking
-    output.innerHTML = "";
+    hazardInstructionsOutput.innerHTML = "";
+    locationContactsOutput.innerHTML = "";
 
     let response = await fetch("data.json");
 
     let data = await response.json();
+
+    for (let location = 0; location < data.locations.length; location++) {
+        let currentLocation = data.locations[location].condition.location;
+
+        if (currentLocation === specificLocation) {
+            for(let contact = 0; contact < data.locations[location].contacts.length; contact++) {
+                let clone = contactlistsTemplate.content.cloneNode(true);
+                clone.querySelector(".contactslist-item-text").innerText = data.locations[location].contacts[contact];
+                locationContactsOutput.appendChild(clone);
+            }
+        }
+    }
 
     for (let action = 0; action < data.actions.length; action++) {
         let currentHazard = data.actions[action].condition.hazard;
 
         if (currentHazard === hazardType) {
             for (let instruction = 0; instruction < data.actions[action].instructions.length; instruction++) {
-            // Lets you duplicate your template and show multiple checklist items.
+                // Creates a copy of the template
                 let clone = checklistTemplate.content.cloneNode(true);
+
+                // Writes the instructions inside the span tag
                 clone.querySelector(".checklist-item-text").innerText = data.actions[action].instructions[instruction];
-                output.appendChild(clone);
+
+                // Adds the copy inside the "#hazard-instructions-output" div
+                hazardInstructionsOutput.appendChild(clone);
+
+                let addedItem = hazardInstructionsOutput.lastElementChild;
+                let checkbox = addedItem.querySelector(".checklist-item-checkbox")
+
+                // removes item after checking its box
+                checkbox.addEventListener("change", function () {
+                    if (checkbox.checked) {
+                        addedItem.remove();
+                    }
+                });
             }
         }
     }
