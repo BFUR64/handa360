b4e5c5d9cffda0196ce1f686f24bbefd208e22e8
Terrance1120 <iterranceevan@gmail.com>
2026-03-29 17:43:14 +0800

    Wrote the initial normalizer for cachedData and get sanitized input

---
+++ experimental/loopTest/normalizer.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+import * as cachedData from "./cachedData.js";
+
+/** @typedef {import("./cachedData.js").Question} Question */
+/** @typedef {import("./cachedData.js").Action} Action */
+/** @typedef {import("./cachedData.js").Location} Location */
+
+/** @returns {Question[]} */
+export function getNormalizedQuestions() {
+    let rawQuestions = cachedData.getQuestions();
+
+    rawQuestions = Array.isArray(rawQuestions) ? rawQuestions : [];
+
+    return rawQuestions.map(question => ({
+        text: typeof question.text === "string" ? question.text : "placeholder text",
+        name: typeof question.name === "string" ? question.name : "placeholder name",
+
+        options: (Array.isArray(question.options) ? question.options : [])
+            .filter(option => typeof option.value === "string" && typeof option.text === "string")
+            .map(option => ({ value: option.value, text: option.text}))
+    }));
+}
+
+/** @returns {Action[]} */
+export function getNormalizedActions() {
+    let rawActions = cachedData.getActions();
+
+    rawActions = Array.isArray(rawActions) ? rawActions : [];
+
+    return rawActions.map(action => ({
+        condition: action.condition && typeof action.condition === "object"
+            ? { hazard: typeof action.condition.hazard === "string" ? action.condition.hazard : "" }
+            : { hazard: "" },
+
+        instructions: (Array.isArray(action.instructions) ? action.instructions : [])
+            .filter(instruction => typeof instruction === "string")
+    }));
+}
+
+/** @returns {Location[]} */
+export function getNormalizedLocations() {
+    let rawLocations = cachedData.getLocations();
+
+    rawLocations = Array.isArray(rawLocations) ? rawLocations : [];
+
+    return rawLocations.map(location => ({
+        condition: location.condition && typeof location.condition === "object"
+            ? { location: typeof location.condition.location === "string" ? location.condition.location : "" }
+            : { location: "" },
+
+        information: (Array.isArray(location.information) ? location.information : [])
+            .filter(information => typeof information === "string")
+    }));
+}
