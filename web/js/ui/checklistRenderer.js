c9fcfa3d06cc4f1319492404f4c4cc21ffd7096e
BFUR64 <iterranceevan@gmail.com>
2026-03-30 22:57:30 +0800

    Organized variables and placed it to the top

--- web/js/ui/checklistRenderer.js
+++ web/js/ui/checklistRenderer.js
@@ -1,5 +1,9 @@
 // @ts-check
 
+const container = /** @type {HTMLElement} */ (document.getElementById("container"));
+const checklistTemplate = /** @type {HTMLTemplateElement} */ (document.getElementById("checklist-template"));
+const checklistItemTemplate = /** @type {HTMLTemplateElement} */ (document.getElementById("checklist-item-template"));
+
 /** @typedef {import("../data/cachedData.js").Action} Action */
 
 /**
@@ -8,22 +12,18 @@
  * @return {HTMLElement}
  */
 export function render(hazard, actions) {
-    let container = /** @type {HTMLElement} */ (document.getElementById("container"));
-    let checklistTemplate = /** @type {HTMLTemplateElement} */ (document.getElementById("checklist-template"));
-    let checklistItemTemplate = /** @type {HTMLTemplateElement} */ (document.getElementById("checklist-item-template"));
-
-    let checklist = /** @type {DocumentFragment} */ (checklistTemplate.content.cloneNode(true));
-    let checklistBlock = /** @type {HTMLElement} */ (checklist.querySelector(".checklist-block"));
+    const checklist = /** @type {DocumentFragment} */ (checklistTemplate.content.cloneNode(true));
+    const checklistBlock = /** @type {HTMLElement} */ (checklist.querySelector(".checklist-block"));
 
     container.innerHTML = "";
 
     actions.forEach(action => {
-        let currentHazard = action.condition.hazard;
+        const currentHazard = action.condition.hazard;
 
         if (currentHazard === hazard) {
             action.instructions.forEach(instruction => {
-                let checklistItem = /** @type {DocumentFragment} */ (checklistItemTemplate.content.cloneNode(true));
-                let checklistText = /** @type {HTMLElement} */ (checklistItem.querySelector(".checklist-item-text"));
+                const checklistItem = /** @type {DocumentFragment} */ (checklistItemTemplate.content.cloneNode(true));
+                const checklistText = /** @type {HTMLElement} */ (checklistItem.querySelector(".checklist-item-text"));
 
                 checklistText.innerText = instruction;
                 checklistBlock.append(checklistItem);
@@ -32,5 +32,6 @@ export function render(hazard, actions) {
             container.append(checklistBlock);
         }
     })
+
     return checklistBlock;
 }
