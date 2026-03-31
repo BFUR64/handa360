d73f957ee1bd0d9afc96a3075bdc8d3387fcb9a9
KiksTin <christinejoyroberto@gmail.com>
2026-03-31 14:34:05 +0800

    Add about and contact sections, Soon: Add Information and Design

--- .experimental/frontendSample/script.js
+++ .experimental/frontendSample/script.js
@@ -1,5 +1,11 @@
 const assessmentForm = document.getElementById('assessmentForm');
+const navAbout = document.getElementById('nav-about');
+const navContact = document.getElementById('nav-contact');
+const navHome = document.getElementById('nav-home');
+
 const report = document.getElementById('report');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
 
 assessmentForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -12,4 +18,23 @@ assessmentForm.addEventListener('submit', function(e) {
 
     console.log('Location:',selectLocation? selectLocation.value: 'None');
     console.log('Hazard:',selectHazard? selectHazard.value: 'None');
+});
+
+navAbout.addEventListener('click', function() {
+    assessmentForm.style.display = 'none';
+    report.classList.remove('show');
+    about.classList.add('show');
+    contact.classList.remove('show');
+});
+navContact.addEventListener('click', function() {
+    assessmentForm.style.display = 'none';
+    report.classList.remove('show');
+    about.classList.remove('show');
+    contact.classList.add('show');
+});
+navHome.addEventListener('click', function() {
+    assessmentForm.style.display = 'flex';
+    report.classList.remove('show');
+    about.classList.remove('show');
+    contact.classList.remove('show');
 });
