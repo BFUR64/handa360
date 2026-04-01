58ff4f3a025cc21d7ac365637e2e37101001b5f8
KiksTin <christinejoyroberto@gmail.com>
2026-04-01 20:32:59 +0800

    Added about and contact section/page, working navigation

---
+++ web/js/ui/pageNavigation.js
@@ -0,0 +1,26 @@
+const navAbout = document.getElementById('navigate-about');
+const navContact = document.getElementById('navigate-contact');
+const navHome = document.getElementById('navigate-home');
+
+const home = document.getElementById('container');
+const about = document.getElementById('about-section');
+const contact = document.getElementById('contact-section');
+
+navHome.addEventListener('click', function(e) {
+    e.preventDefault();
+    about.classList.remove('show');
+    contact.classList.remove('show');
+    home.style.display = 'block';
+});
+navAbout.addEventListener('click', function(e) {
+    e.preventDefault();
+    about.classList.add('show');
+    contact.classList.remove('show');
+    home.style.display = 'none';
+});
+navContact.addEventListener('click', function(e) {
+    e.preventDefault();
+    about.classList.remove('show');
+    contact.classList.add('show');
+    home.style.display = 'none';
+});
