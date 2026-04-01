fafc1524fe4783e105fbb1305112d953381af88d
KiksTin <christinejoyroberto@gmail.com>
2026-04-01 21:01:16 +0800

    Added footer

--- web/js/ui/pageNavigation.js
+++ web/js/ui/pageNavigation.js
@@ -3,6 +3,7 @@ const navContact = document.getElementById('navigate-contact');
 const navHome = document.getElementById('navigate-home');
 
 const home = document.getElementById('container');
+const footer = document.getElementById('footer');
 const about = document.getElementById('about-section');
 const contact = document.getElementById('contact-section');
 
@@ -10,17 +11,20 @@ navHome.addEventListener('click', function(e) {
     e.preventDefault();
     about.classList.remove('show');
     contact.classList.remove('show');
+    footer.classList.remove('show');
     home.style.display = 'block';
 });
 navAbout.addEventListener('click', function(e) {
     e.preventDefault();
     about.classList.add('show');
     contact.classList.remove('show');
+    footer.classList.add('show');
     home.style.display = 'none';
 });
 navContact.addEventListener('click', function(e) {
     e.preventDefault();
     about.classList.remove('show');
     contact.classList.add('show');
+    footer.classList.add('show');
     home.style.display = 'none';
 });
