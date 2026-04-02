30cefa8533b9ad9fad085899cb6550212eaa1739
KiksTin <christinejoyroberto@gmail.com>
2026-04-02 18:08:46 +0800

    Add button for light and dark mode

--- web/js/ui/pageNavigation.js
+++ web/js/ui/pageNavigation.js
@@ -30,3 +30,11 @@ navContact.addEventListener('click', function (e) {
     footer.classList.add('show');
     home.style.display = 'none';
 });
+
+function toggleTheme() {
+        const mode = /** @type {HTMLImageElement} */ (document.getElementById('theme-toggle'));
+        document.body.classList.toggle('dark');
+        if(mode.src.match('light-assets/images/light-mode.png.png')){
+            mode.src = ('assets/images/dark-mode.png');
+        }else{mode.src = ('assets/images/light-mode.png');}
+}
