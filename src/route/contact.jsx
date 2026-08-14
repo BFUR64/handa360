996ef33826eb315a4f91234bd4ffb656fafd2e3b
BFUR64 <iterranceevan@gmail.com>
2026-08-14 15:02:49 +0800

    feat: Enhance Contact page with detailed information and styling updates

--- src/route/contact.jsx
+++ src/route/contact.jsx
@@ -6,7 +6,25 @@ import './css/contact.css';
 export default function Contact () {
     return (
         <main className="main-contact">
-            <p>Hello World!</p>
+            <div className="contact-title">
+                <h2>Contact Handa360</h2>
+            </div>
+
+            <div className="contact-information">
+                <h3><a href="https://github.com/BFUR64/handa360">Handa360 GitHub</a></h3>
+            </div>
+
+            <div className="contact-information">
+                <h3>Terrance Evan I. Clark</h3>
+                <h4>(Systems Architect & Lead Logic Developer)</h4>
+            </div>
+            <ul className="contact-list">
+                <li><a href="mailto:iterranceevan@gmail.com">iterranceevan@gmail.com</a></li>
+                <li><a href="tel:+639128798083">+63 912 8798 083</a></li>
+                <li><a href="https://github.com/BFUR64/">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/terrance-clark-b586723b5/">LinkedIn</a></li>
+                <li><a href="https://discord.com/users/285707454568988673">Discord</a></li>
+            </ul>
         </main>
     );
 }
