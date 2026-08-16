ecf4de3c305560017283f6ba75a4add6c9e98c72
BFUR64 <iterranceevan@gmail.com>
2026-08-16 11:43:16 +0800

    feat: Add hamburger menu

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -3,18 +3,40 @@
 
 import { useNavigate } from 'react-router-dom';
 import './css/nav.css';
+import { useState } from 'react';
 
 export default function Nav () {
     const navigate = useNavigate();
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    /**
+     * @param {string} path
+     */
+    function navigateAndClose(path) {
+        navigate(path);
+        setMenuOpen(false);
+    }
 
     return (
         <header>
             <img data-src="handa360-logo.webp" loading="lazy" decoding="async" alt="Handa360" />
             <nav>
-                <ul className="nav-list">
-                    <li><button type="button" onClick={() => navigate("/")}>Home</button></li>
-                    <li><button type="button" onClick={() => navigate("/about")}>About</button></li>
-                    <li><button type="button" onClick={() => navigate("/contacts")}>Contacts</button></li>
+                <button
+                    className="btn-generic"
+                    aria-label="Toggle navigation"
+                    aria-expanded={menuOpen}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >☰</button>
+                <button className={
+                    menuOpen ? "nav-overlay nav-open" : "nav-overlay"}
+                    aria-label="Close navigation"
+                    onClick={() => setMenuOpen(false)}
+                ></button>
+                <ul className={menuOpen ? "nav-list nav-open" : "nav-list"}>
+                    <li><button type="button" onClick={() => navigateAndClose("/")}>Home</button></li>
+                    <li><button type="button" onClick={() => navigateAndClose("/about")}>About</button></li>
+                    <li><button type="button" onClick={() => navigateAndClose("/contacts")}>Contacts</button></li>
                 </ul>
             </nav>
         </header>
